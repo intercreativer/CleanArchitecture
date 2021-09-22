b26a379b2045c1f6b0fee7afc44b1506018d4113
Igor Antonov <5915087@ups.com>
2021-09-22 16:23:33 -0400

    Creating new MVC project

---
+++ CleanArch/CleanArchitecture.Mvc/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    public class OrderController : Controller
+    {
+        private iOrderService _orderService;
+        public OrderController(iOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        public IActionResult Index()
+        {
+            OrderViewModel model = _orderService.GetOrders();
+            return View(model);
+        }
+    }
+}
---
+++ CleanArch/Mvc.CleanArch/Data/Migrations/20210922111400_InitialMigrationIdentityDBforMVC.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mvc.CleanArch.Data.Migrations
+{
+    public partial class InitialMigrationIdentityDBforMVC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
